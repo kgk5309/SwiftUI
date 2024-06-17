be38681a854d2b59aadf65dbf484e390fbf616aa
kgk5309 <64968901+kgk5309@users.noreply.github.com>
2024-06-17 15:02:15 +0900

    1.0
    기본문법 print

---
+++ playground/print.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+print(greeting) // Hello, playground 출력
+print("greeting") // greeting 출력
+
+greeting = "Hello, Swift"
+print(greeting) // Hello, Swift 출력
